2348dd9e668bc6acc2f742d89723addba5708121
renanstopa <RenanStopa@outlook.com>
2020-10-21 17:45:46 -0300

    Adição da classe de comandos gerais do usuário e criação do método de verificar login

---
+++ prjCuidaEmCasa/classes/Agendamento/clsUsuario.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace prjCuidaEmCasa.classes.Agendamento
+{
+    public class clsUsuario: clsBanco_32623 
+    {
+        public string emailUsuario { get; set; }
+        public string tipoUsuario { get; set; }
+
+        public clsUsuario(): base()
+        {
+            emailUsuario = "";
+            tipoUsuario = "";
+        }
+
+        #region Verificar Login Usuario
+        public bool verificarLogin(string emailUsuario, string senhaUsuario)
+        {
+            MySqlDataReader dados = null;
+            string[,] valores = new string[2, 2];
+            valores[0, 0] = "vEmailUsuario";
+            valores[0, 1] = emailUsuario;
+            valores[1, 0] = "vSenha";
+            valores[1, 1] = senhaUsuario;
+            
+            if (!Procedure("verificarLogin", true, valores, ref dados))
+            {
+                Desconectar();
+                return false;
+            }
+
+            if (dados.HasRows)
+            {
+                while (dados.Read())
+                {
+                    emailUsuario = dados[0].ToString();
+                    tipoUsuario = dados[1].ToString();
+                }
+
+                if (!dados.IsClosed) { dados.Close(); }
+                Desconectar();
+            }
+
+            return true;
+        }
+        #endregion 
+    }
+}
