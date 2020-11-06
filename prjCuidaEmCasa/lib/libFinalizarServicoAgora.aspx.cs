877c715cfd67448c46d8338f2f9ca571e93a1f96
Kauê Gatto <kauegatto123@gmail.com>
2020-11-06 16:49:36 -0300

    Revert "Revert "Merge branch 'main' of https://github.com/kauegatto/Cuida-Em-Casa into main""
    This reverts commit dff7008290134028be5264a29faaa946ec85433b.

---
+++ prjCuidaEmCasa/lib/libFinalizarServicoAgora.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using prjCuidaEmCasa.classes.Agendamento;
+
+namespace prjCuidaEmCasa.lib
+{
+    public partial class libFinalizarServicoAgora : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            clsServico servico = new clsServico();
+
+            #region Variáveis com dados para realização do agendamento
+            servico.proxCodigo();
+            string proxCodigo = servico.codigo;
+
+            string dtInicio = Request["dtInicioServico"].ToString();
+            string hrInicio = Request["horaInicio"].ToString();
+            string hrFim = Request["horaFim"].ToString();
+            string cep = Request["cep"].ToString();
+            string cidade = Request["cidade"].ToString();
+            string bairro = Request["bairro"].ToString();
+            string rua = Request["rua"].ToString();
+            string num = Request["num"].ToString();
+            string uf = Request["estado"].ToString();
+            string comp = Request["comp"].ToString();
+            string emailCliente = Request["cliente"].ToString();
+            string cdPaciente = Request["cdPaciente"].ToString();
+            #endregion
+
+            bool viraDia = Convert.ToBoolean(Session["viraDia"]);
+            if (!(servico.finalizarServicoAgora(proxCodigo, dtInicio, hrInicio, hrFim, cep, cidade, bairro, rua, num, uf, comp, emailCliente, cdPaciente, viraDia)))
+            {
+                Response.Write("false");
+            }
+
+            Response.Write("true");
+        }
+    }
+}
