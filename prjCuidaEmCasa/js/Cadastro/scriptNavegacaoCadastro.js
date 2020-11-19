4e969d4063fe49fd4154f5e06a4bd6b2430e6a64
Douglas Reis <59988180+dougreisss@users.noreply.github.com>
2020-11-19 14:31:43 -0300

    navegacao cadastro

---
+++ prjCuidaEmCasa/js/Cadastro/scriptNavegacaoCadastro.js
@@ -0,0 +1,29 @@
+﻿import scriptCriarContaClinte from './scriptCriarContaClinte.js';
+
+$(document).ready(function(){
+
+	$(document).on("click", "#btnProximo", function(){
+
+		// criar validacao
+
+		localStorage.setItem("nomeCliente", $("#txtNomeCliente").val());
+		localStorage.setItem("emailCliente", $("#txtEmailCliente").val());
+		localStorage.setItem("telefoneCliente", $("#txtTelefoneCliente").val());
+
+		$("#wrapper-CadastroPrimeiro").css("display","none");
+
+        $("#wrapper-CadastroSegundo").addClass("visivel");
+	});
+
+	$(document).on("click", "#btnCadastrar", function(){
+
+		localStorage.setItem("cpfCliente", $("#txtCPF").val());
+		localStorage.setItem("senhaCliente", $("#txtSenha").val());
+		localStorage.setItem("confirmarSenhaCliente", $("#txtConfirmarSenha").val());
+
+		window.location.href = "../../pages/cliente/atendimento.html";
+
+	});
+
+
+});
