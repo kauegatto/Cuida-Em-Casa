25f465a4ab58a2874ebd8b937c93bb30171c9ec0
Douglas Reis <59988180+dougreisss@users.noreply.github.com>
2020-11-28 14:35:39 -0300

    Envio de email após a remoção de suspensão do cuidador funcionando

--- prjCuidaEmCasa/lib/libSuspenderBanir.aspx.cs
+++ prjCuidaEmCasa/lib/libSuspenderBanir.aspx.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using prjCuidaEmCasa.classes;
+using System.Net.Mail;
+using System.Net;
 
 namespace prjCuidaEmCasa.lib
 {
@@ -41,7 +43,25 @@ namespace prjCuidaEmCasa.lib
             string emailCuidador = Request["emailCuidador"].ToString();
             string control = Request["control"].ToString();
 
+            #region CONTA REMETENTE
+            string remetente = "3n2k20@gmail.com"; //tirar antes de dar commit
+            string senha = "vencedordopovo123";    //tirar antes de dar commit
+            #endregion
+
+            #region Configuração do Remetente
+            SmtpClient client = new SmtpClient();
+
+            client.Credentials = new NetworkCredential(remetente, senha);
+            #endregion
+
+            #region Configuração do Servidor SMTP do GMAIL
+            client.Host = "smtp.gmail.com";
+            client.Port = 587;
+            client.EnableSsl = true;
+            #endregion
+
             clsAdministrador adm = new clsAdministrador();
+            MailMessage mandarEmail = new MailMessage();
 
             if (control == "0")
             {
@@ -60,6 +80,29 @@ namespace prjCuidaEmCasa.lib
                     if (!(adm.removerSuspensao(emailCuidador)))
                     {
                         Response.Write("false");
+                        return;
+                    }
+
+                    mandarEmail.To.Add(emailCuidador);
+                    mandarEmail.From = new MailAddress(remetente, "Contato Cuida Em Casa", System.Text.Encoding.UTF8);
+                    mandarEmail.Subject = "Remoção de Suspensão";
+                    mandarEmail.SubjectEncoding = System.Text.Encoding.UTF8;
+
+                    string conteudo = "<html><body>A sua suspensão foi retirada, agora você está apto para exercer a sua cuidadoria em nosso aplicativo";
+                    conteudo += "</body></html>";
+
+                    mandarEmail.Body = conteudo;
+                    mandarEmail.BodyEncoding = System.Text.Encoding.UTF8;
+                    mandarEmail.IsBodyHtml = true;
+                    mandarEmail.Priority = MailPriority.High;
+
+                    try
+                    {
+                        client.Send(mandarEmail);
+                    }
+                    catch (Exception)
+                    {
+                        Response.Write("erro");
                         return;
                     }
 
